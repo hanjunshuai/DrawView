bf89582329c6bad6d44b332827f624ee958b8bf2
unknown <1433452759@qq.com>
2018-06-12 13:57:11 +0800

    画点

---
+++ app/src/main/java/hjs/zhi/com/drawview/DrawPointAndLineActivity.java
@@ -0,0 +1,57 @@
+package hjs.zhi.com.drawview;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class DrawPointAndLineActivity extends AppCompatActivity {
+
+    private ImageView mPointImageView, mLineImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_point_and_line);
+
+        mPointImageView = findViewById(R.id.iv_point);
+        mLineImageView = findViewById(R.id.iv_line);
+
+        doDrawPoint();
+        doDrawLine();
+    }
+
+    private void doDrawLine() {
+
+    }
+
+    Bitmap.Config mConfig = Bitmap.Config.ARGB_8888;
+    int mWidth = 300;
+    int mHeight = 300;
+
+    private void doDrawPoint() {
+
+        Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, mConfig);
+
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.parseColor("#666666"));
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(5);
+
+        float[] pts = new float[]{
+                20, 40,
+                40, 40,
+                60, 60,
+                80, 80,
+                100, 100,
+        };
+        canvas.drawPoints(pts, paint);
+
+        mPointImageView.setImageBitmap(bitmap);
+    }
+}
